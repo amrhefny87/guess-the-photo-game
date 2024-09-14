6ddb4990535b37b8e23efd10ae58f2da7464fd04
amrhefny1987 <amr.hefny@factoriaf5.org>
2024-09-14 19:57:34 +0200

    first commit

---
+++ src/main/java/personal/game/guessThePhoto/GuessThePhotoApplication.java
@@ -0,0 +1,13 @@
+package personal.game.guessThePhoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessThePhotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessThePhotoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/personal/game/guessThePhoto/GuessThePhotoApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.game.guessThePhoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessThePhotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
